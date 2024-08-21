d26ee9779a70c2fb6790f06af3db296fc901ea6e
Chad Messimer <103394173+cmessimer@users.noreply.github.com>
2024-08-21 11:23:40 -0400

    Create day-6-elif-demo.py

---
+++ day/6/day-6-elif-demo.py
@@ -0,0 +1,11 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "chad":
+  print("Hello Chad")
+elif username == "":
+  print("Invalid login. please try again")
+else:
+  print("Go away!")
