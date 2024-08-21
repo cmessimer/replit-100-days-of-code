cf44558f5a0e3ca230521ea2c8cde4411ede20ad
Chad Messimer <103394173+cmessimer@users.noreply.github.com>
2024-08-21 11:08:51 -0400

    Create day-5-if-else-challenge.py

---
+++ day/5/day-5-if-else-challenge.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print("\n")
+spiderman = input("Do you like 'hanging' around? ")
+if spiderman == "no":
+  print("Then you're not Spider-man ")
+else:
+  print("You might be Spiderman")
+korg = input("Do you have a 'gravelly' voice? ")
+if korg == "no":
+  print("Aww, then you're not Korg ")
+else:
+  print("Then you might be Korg")
+marvel = input("Do you often feel 'Marvelous'? ")
+if marvel == "yes":
+  print("Aha! You're Captain Marvel! Hi! ")
+else:
+  print("You're not Captain Marvel")
