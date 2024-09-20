c090ce15da3f9dfc82db1c2bc2a9d4f847c6635a
Chad Messimer <103394173+cmessimer@users.noreply.github.com>
2024-09-20 16:38:17 -0400

    Create Create day-7-nesting-challenge.py

---
+++ day/7/Create day-7-nesting-challenge.py
@@ -0,0 +1,14 @@
+tvShow = input("What is your favourite Anime? ")
+if tvShow == "One Peice":
+  oneCharacter = input("Oh really!? Name me any of the characters? ")
+  if oneCharacter == "Nami":
+    print("You got that by pure chance. Okay then, what is her job on the ship?")
+    firstBounty = input("What was her first bounty then?")
+    if firstBounty == "Navigator":
+      print("Wow, a real fan!")
+    else:
+      print("See! FAKE One Peice fan!")
+elif tvShow == "Succession":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all...")
